499d2328fdc18d1e4d3eac4abf240ccc727a5451
tentenball <lth8905@gmail.com>
2024-11-26 00:50:08 +0900

    style: 2606번 설계

---
+++ code/dfsbfs/dfsbfs2606.java
@@ -0,0 +1,23 @@
+package code.dfsbfs;
+
+public class dfsbfs2606 {
+    static int year = 0;
+    public static void main(String[] args) {
+        // 가로 세로 입력
+
+        // 빙하높이 입력
+
+        // 인접한 바다 개수 할당(2차원 배열)
+        while (true){
+            // year 더하기
+            // year * 바다개수만큼 빼기 (음수가 되면 0으로 바꿉니다)
+
+            // dfs로 만약 count가 1 초과면 break;
+
+            // 다 0이면 year 0으로 하고 break;
+            break;
+        }
+        System.out.println(year);
+    }
+
+}
