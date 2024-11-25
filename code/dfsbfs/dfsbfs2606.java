4353627dc8976045c2deb9b7588c9c8ab6e08ad3
tentenball <lth8905@gmail.com>
2024-11-26 01:32:25 +0900

    feat: 2606번 입력 구현

--- code/dfsbfs/dfsbfs2606.java
+++ code/dfsbfs/dfsbfs2606.java
@@ -1,14 +1,44 @@
 package code.dfsbfs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class dfsbfs2606 {
     static int year = 0;
-    public static void main(String[] args) {
-        // 가로 세로 입력
+    static int width, height;
+    static int[][] iceberg;
+    static int[][] seaCount;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input);
+
+        height = Integer.parseInt(st.nextToken());
+        width = Integer.parseInt(st.nextToken());
+        iceberg = new int[height][width];
+        for (int i = 0; i < height; i++) {
+            input = br.readLine();
+            st = new StringTokenizer(input);
+            for (int j = 0; j < width; j++) {
+                iceberg[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(iceberg[i][j]);
+            }
+            System.out.println();
+        }
+        ;
 
         // 빙하높이 입력
 
         // 인접한 바다 개수 할당(2차원 배열)
-        while (true){
+        while (true) {
             // year 더하기
             // year * 바다개수만큼 빼기 (음수가 되면 0으로 바꿉니다)
 
